f734d999949b7366547c4260eed6ec1610e66b3b
astomber <98065471+astomber@users.noreply.github.com>
2022-07-24 12:25:50 -0500

    v1

---
+++ portfolio-personal-app/pages/_document.tsx
@@ -0,0 +1,14 @@
+import {Html, Head, Main, NextScript} from 'next/document'
+//dark theme
+export default function Document() {
+  return (
+    <Html className =  'dark'>
+      <Head>
+      </Head>
+      <body>
+      <Main/>
+        <NextScript/>
+      </body>
+      </Html>
+  )
+}
